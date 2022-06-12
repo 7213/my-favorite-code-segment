2cd7631abbdcc55fe66bbc6877080df51b152a7c
7213 <13720019822@163.com>
2022-06-12 22:35:26 +0800

    feat: 学习过程宏

--- rust-demo/rust-note/custom-derive/src/lib.rs
+++ rust-demo/rust-note/custom-derive/src/lib.rs
@@ -1,19 +1,22 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::DeriveInput;
+use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(hello_macro)]
-pub fn my_macro(input: TokenStream) -> TokenStream {
+pub fn hello_macro(input: TokenStream) -> TokenStream {
     // Parse the tokens into a syntax tree
-    let ast: DeriveInput = syn::parse(input).unwrap();
+    let ast = parse_macro_input!(input as DeriveInput);
+    let ident = ast.ident;
 
     // Build the output, possibly using quasi-quotation
     let expanded = quote! {
-        /* ... */
+        trait Hello {
+            fn say_hello(&self) {
+                println!("{}", "hello_word2")
+            }
+        }
+        impl Hello for #ident {};
     };
-
-    println!("{}", "过程宏插入");
-
     // Convert into a token stream and return it
     expanded.into()
 }
--- rust-demo/rust-note/src/syn_quote_example.rs
+++ rust-demo/rust-note/src/syn_quote_example.rs
@@ -1,20 +1,21 @@
 extern crate custom_derive;
-use custom_derive::hello_macroß;
-
-#[derive(hello_macro)]
-pub(crate) struct MySelf {
-    name: String,
-    age: u8,
-}
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_sync() {
-        use crate::syn_quote_example::MySelf;
+        use custom_derive::hello_macro;
+
+        #[derive(hello_macro)]
+        pub(crate) struct MySelf {
+            name: String,
+            age: u8,
+        }
+
         let me = MySelf {
             name: String::from("sz"),
             age: 18,
-  ß      };
+        };
+        me.say_hello();
     }
 }
