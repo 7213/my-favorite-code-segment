f9593a45c8b1fd108d3164f0a7a63d52a87bb343
7213 <13720019822@163.com>
2022-07-10 22:18:41 +0800

    feat: add debug swc

---
+++ rust-demo/debug-swc/src/main.rs
@@ -0,0 +1,42 @@
+#[macro_use]
+extern crate swc_common;
+extern crate swc_ecma_parser;
+use swc_common::sync::Lrc;
+use swc_common::{
+    errors::{ColorConfig, Handler},
+    FileName, FilePathMapping, SourceMap,
+};
+use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
+
+fn main() {
+    let cm: Lrc<SourceMap> = Default::default();
+
+    // Real usage
+    // let fm = cm
+    //     .load_file(Path::new("test.js"))
+    //     .expect("failed to load test.js");
+    let fm = cm.new_source_file(
+        FileName::Custom("test.js".into()),
+        "function foo() {}".into(),
+    );
+    let lexer = Lexer::new(
+        // We want to parse ecmascript
+        Syntax::Es(Default::default()),
+        // EsVersion defaults to es5
+        Default::default(),
+        StringInput::from(&*fm),
+        None,
+    );
+
+    let mut parser = Parser::new_from(lexer);
+
+    for e in parser.take_errors() {
+    }
+
+    let _module = parser
+        .parse_module()
+        .map_err(|mut e| {
+            // Unrecoverable fatal error occurred
+        })
+        .expect("failed to parser module");
+}
