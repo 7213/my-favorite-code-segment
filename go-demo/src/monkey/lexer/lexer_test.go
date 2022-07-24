d6535b8ddd55d3d765fe80d09fb61ba6d0b8c271
7213 <bill.sz@alibaba-inc.com>
2022-07-24 14:12:55 +0800

    feat: go语言自制编译器练习

--- go-demo/src/monkey/lexer/lexer_test.go
+++ go-demo/src/monkey/lexer/lexer_test.go
@@ -28,7 +28,7 @@ func TestNextToken(t *testing.T) {
 	l := New(input)
 
 	for i, tt := range tests {
-		tok = l.NextToken()
+		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokenType wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
